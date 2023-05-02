32d8773665deb5c942a208c8f07e6a32f35bc627
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-01 15:18:21 -1100

    Display message for empty input

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -3,18 +3,25 @@ import { useState } from "react";
 
 const InputTodo = ({ addTodoItem }) => {
   const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('')
 
   const handleChange =(e) => {
     setTitle(e.target.value);
   }
 
   const handleSubmit =(e) => {
+    if (title.trim()) {
     e.preventDefault();
     addTodoItem(title);
     setTitle('')
+    setMessage('')
+    }else {
+      setMessage('Please add an item')
+    }
   }
 
   return (
+    <>
     <form onSubmit={handleSubmit}>
 
       <input 
@@ -25,6 +32,8 @@ const InputTodo = ({ addTodoItem }) => {
 
       <button>Submit</button>
     </form>
+    <span>{message}</span>
+    </>
   );
 };
 export default InputTodo;
