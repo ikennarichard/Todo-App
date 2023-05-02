b87a2e7dc5534da97c1afe305275145fdbc9ab75
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-01 15:57:35 -1100

    Add classnames for elements

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -22,17 +22,20 @@ const InputTodo = ({ addTodoItem }) => {
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
+    <form 
+    onSubmit={handleSubmit}
+    className="form-container">
 
-      <input 
+      <input
+      className="input-text" 
       type="text" 
       placeholder="Add Todo..." 
       value={title}
       onChange={handleChange}/>
 
-      <button>Submit</button>
+      <button className="input-submit">Submit</button>
     </form>
-    <span>{message}</span>
+    <span className="submit-warning">{message}</span>
     </>
   );
 };
