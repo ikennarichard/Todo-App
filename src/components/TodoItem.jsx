b798fd67e113e661f9c059cb7b96ebe2173959c6
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-01 16:07:04 -1100

    Update todo item styles

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,14 +1,18 @@
-/*eslint-disable*/
+/* eslint-disable react/prop-types */
+import styles from './TodoItem.module.css'
+
 export default function TodoItem({ itemProp, handleChange, delTodo }) {
   return (
     
-    <li>
-      <input 
-      type="checkbox" 
-      checked={itemProp.completed}
-      onChange={() => handleChange(itemProp.id)}/>
-      <button onClick={() => delTodo(itemProp.id)}>Delete</button>
-      {itemProp.title}
+    <li className={styles.item}>
+      <div className={styles.content}>
+        <input 
+        type="checkbox" 
+        checked={itemProp.completed}
+        onChange={() => handleChange(itemProp.id)}/>
+        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+        {itemProp.title}
+      </div>
     </li>
   )
 }
